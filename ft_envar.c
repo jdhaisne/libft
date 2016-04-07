a098c24fdac3b64a9db3638c0420a24e12f7b41c
Julien DHAISNE <jdhaisne@e1r5p22.42.fr>
2016-04-07 16:06:18 +0200

    070416

--- ft_envar.c
+++ ft_envar.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_envar.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdhaisne <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/06 10:56:05 by jdhaisne          #+#    #+#             */
+/*   Updated: 2016/04/06 10:56:47 by jdhaisne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char *ft_envar(const char *name, t_list *env_l)
+char	*ft_envar(const char *name, t_list *env_l)
 {
 	char	*var;
 
 	var = NULL;
-	while (env_l != NULL && ft_strncmp(env_l->content, name, 
+	while (env_l != NULL && ft_strncmp(env_l->content, name,
 		ft_strlenc(env_l->content, '=')) != 0)
 		env_l = env_l->next;
 	if (env_l == NULL)
--- ft_lstdup.c
+++ ft_lstdup.c
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdhaisne <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/06 10:57:22 by jdhaisne          #+#    #+#             */
+/*   Updated: 2016/04/06 12:16:53 by jdhaisne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 t_list	*ft_lstdup(t_list *start)
 {
 	t_list	*new_start;
-	t_list *tmp1;
-	t_list *tmp2;
+	t_list	*tmp1;
+	t_list	*tmp2;
 
+	if(start == NULL)
+		return (NULL);
 	new_start = ft_lstnew(start->content, start->content_size);
 	tmp1 = new_start;
 	tmp2 = start;
-	while(tmp2->next != NULL)
+	while (tmp2->next != NULL)
 	{
 		tmp2 = tmp2->next;
 		tmp1->next = ft_lstnew(tmp2->content, tmp2->content_size);
